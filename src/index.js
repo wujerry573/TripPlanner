e6f07b572f60cfed1cb64d92c22a5ae06119a056
veryspry <ehlinger.matt@gmail.com>
2018-06-19 15:01:13 -0500

    map working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('heyeyeyeyeyeyeyeyeyye')
+
+
+
+
+
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidmVyeXNwcnkiLCJhIjoiY2ppbTN2c3dnMDAzbTNxbzJqN2xlOTlmcCJ9.La_7vHSyG2W6g0etA5YqXQ';
+var map = new mapboxgl.Map({
+  container: document.getElementById('map'),
+  style: 'mapbox://styles/mapbox/streets-v10'
+});
